5f7a0e17de9e0f9ed86836623d0f7de6824c3f89
mostafa99hosny <m.hosney33@gmail.com>
2025-05-24 03:51:41 +0300

    finishing

--- src/app/pages/products/product-form/product-form.component.ts
+++ src/app/pages/products/product-form/product-form.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SharedCardComponent } from "../../../shared/shared-card/shared-card.component";
-import { Router } from '@angular/router';
+import { Router ,ActivatedRoute } from '@angular/router';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DynamicProductService } from '../../../services/dynamic-product.service';
@@ -12,8 +12,32 @@ import { DynamicProductService } from '../../../services/dynamic-product.service
   styleUrl: './product-form.component.css'
  
 })
-export class ProductFormComponent {
-  constructor(private router:Router,private productService :DynamicProductService){}
+export class ProductFormComponent implements OnInit,OnDestroy {
+  productId: any;
+  constructor(private router:Router,private productService :DynamicProductService ,private activeRoute:ActivatedRoute){}
+  ngOnDestroy(): void {
+    console.log('ProductFormComponent destroyed');
+  }
+
+  ngOnInit(): void {
+    this.activeRoute.paramMap.subscribe({
+      next: (params) => {
+        this.productId = params.get('id') || 0; 
+        this.productForm.reset(); 
+
+        if (this.productId != 0) {
+          this.productService.getProductById(this.productId).subscribe((response) => {
+            this.getName.setValue(response.name);
+            this.getPrice.setValue(response.price.toString());
+            this.getQuanntity.setValue(response.quantity.toString());
+          });
+        }
+      },
+      error: (err) => {
+        console.error('Error fetching route parameters:', err);
+      },
+    });
+  }
   productForm = new FormGroup({
     name :new FormControl('',[Validators.required,Validators.minLength(3)]),
     price :new FormControl('',[Validators.required]),
@@ -29,15 +53,21 @@ export class ProductFormComponent {
     return this.productForm.controls['quantity'];
   }
   productHandler(e:Event){
+    e.preventDefault();
     if(this.productForm.status =='VALID'){
-      this.productService.addNewProduct(this.productForm.value).subscribe({
-        next:()=>{
+      if(this.productId ==0){
+        this.productService.addNewProduct(this.productForm.value).subscribe((response)=>{
           this.router.navigate(['/products']);
-        }
-      })
+        })
+      }
+      else{
+        this.productService.editProduct(this.productId,this.productForm.value).subscribe((response)=>{
+          this.router.navigate(['/products']);
+        })
+      }
     }
     else{
-      console.log('fix validation of forms');
+      this.productForm.markAllAsTouched();
       
     }    
   }
